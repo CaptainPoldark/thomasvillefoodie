fa20a7f99629c951c6ce91340dc213888ad1abd5
CaptainPoldark <61985921+CaptainPoldark@users.noreply.github.com>
2022-03-18 13:01:27 -0400

    Update apolloHelper.js

--- components/apolloHelper.js
+++ components/apolloHelper.js
@@ -6,7 +6,7 @@ import {
 } from "@apollo/client";
 
 function ApolloHelper() {
-  const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://159.223.140.68:1337";
+  const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://foodiedb.battlegroundls.com";
   const link = new HttpLink({ uri: `${API_URL}/graphql` });
   const cache = new InMemoryCache();
   const client = new ApolloClient({ link, cache });
